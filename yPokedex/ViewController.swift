cd878866672bbfedd0ac0ea3c40d4fb8e8dafedf
jsoliman2017 <solimanjuanito@yahoo.com>
2017-05-29 17:14:13 +0800

    Initial Commit

---
+++ yPokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yPokedex
+//
+//  Created by Juanito Soliman on 29/05/2017.
+//  Copyright © 2017 Juanito Soliman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
